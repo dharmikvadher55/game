27906154d1724550256098e1087e12c70c7b8c55
Dharmikvadher55 <127009141+dharmikvadher55@users.noreply.github.com>
2024-07-18 22:39:03 +0530

    Update

---
+++ config.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyA37JyFHuUay7guLz31z24MIfbhYib2Lao",
+  authDomain: "qurious-steel.firebaseapp.com",
+  projectId: "qurious-steel",
+  storageBucket: "qurious-steel.appspot.com",
+  messagingSenderId: "835752084298",
+  appId: "1:835752084298:web:40a9d23efd2206dd3137b3",
+  measurementId: "G-0L59RLYX9S"
+};
