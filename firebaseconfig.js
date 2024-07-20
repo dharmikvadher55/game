cb666d1daa41a656e4c331a05bf083b4a1aa625b
Dharmikvadher55 <127009141+dharmikvadher55@users.noreply.github.com>
2024-07-20 20:19:08 +0530

    Add files via upload

---
+++ firebaseconfig.js
@@ -0,0 +1,15 @@
+// config.js
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+ const firebaseConfig = {
+  apiKey: "AIzaSyCJG1luU9eFXdvHeCb840dac0bqp7H08-g",
+  authDomain: "tictac-2342.firebaseapp.com",
+  databaseURL: "https://tictac-2342-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "tictac-2342",
+  storageBucket: "tictac-2342.appspot.com",
+  messagingSenderId: "97994082367",
+  appId: "1:97994082367:web:c0614c3edcebe78b86508a",
+  measurementId: "G-RXQJ1J0JTJ"
+};
+
+export default firebaseConfig;
+
